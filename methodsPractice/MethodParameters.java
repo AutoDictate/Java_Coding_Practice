519e00de54a059a3d8bb6aa07c58efd447d6e9ae
Jayasurya A <jayasurya0206@gmail.com>
2023-01-31 13:46:28 +0530

    Simple Methods code

---
+++ methodsPractice/MethodParameters.java
@@ -0,0 +1,9 @@
+package methodsPractice;
+
+public class MethodParameters {
+
+	public static void main(String[] args){
+		
+		int string;
+	}
+}
---
+++ methodsPractice/SimpleMethod.java
@@ -0,0 +1,19 @@
+package methodsPractice;
+
+public class SimpleMethod {
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello this is your: ");
+		name();
+	}
+	
+	public static void name(){
+		System.out.println("Surya");
+		good();
+	}
+	static void good(){
+		System.out.println("He is a Lazy Boy");
+	}
+}
+
